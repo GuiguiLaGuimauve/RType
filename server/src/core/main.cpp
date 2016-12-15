f41e0284c72479b60abb81844155c562781eff74
Maxime Lecoq <maxime.lecoq@epitech.eu>
2016-12-15 16:00:35 +0100

    correction d'une merde

--- server/src/core/main.cpp
+++ server/src/core/main.cpp
@@ -5,10 +5,10 @@
 // Login   <maxime.lecoq@epitech.eu>
 // 
 // Started on  Thu Dec 15 14:47:53 2016 Maxime Lecoq
-// Last update Thu Dec 15 14:49:10 2016 lecoq
+// Last update Thu Dec 15 15:59:54 2016 lecoq
 //
 
-#include "CoreServer.cpp"
+#include "CoreServer.hh"
 
 int	main()
 {
