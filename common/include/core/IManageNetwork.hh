ed2a51534a0be04febac6756f756eb1cca2f187f
Maxime Lecoq <maxime.lecoq@epitech.eu>
2016-12-15 14:47:06 +0100

    ajout de faux fichier en attendant le reseau

---
+++ common/include/core/IManageNetwork.hh
@@ -0,0 +1,16 @@
+//
+// IManageNetwork.hh for IManageNetwork in /home/lecoq_m/RType
+// 
+// Made by Maxime Lecoq
+// Login   <maxime.lecoq@epitech.eu>
+// 
+// Started on  Thu Dec 15 14:45:38 2016 Maxime Lecoq
+// Last update Thu Dec 15 14:45:48 2016 lecoq
+//
+
+#ifndef IMANAGENETWORK_HH_
+# define IMANAGENETWORK_HH_
+
+class IManageNetwork {};
+
+#endif /* !IMANAGENETWORK_HH_ */
---
+++ common/include/core/IUserNetwork.hh
@@ -0,0 +1,16 @@
+//
+// IUserNetwork.hh for IUserNetwork in /home/lecoq_m/RType
+// 
+// Made by Maxime Lecoq
+// Login   <maxime.lecoq@epitech.eu>
+// 
+// Started on  Thu Dec 15 14:45:52 2016 Maxime Lecoq
+// Last update Thu Dec 15 14:45:58 2016 lecoq
+//
+
+#ifndef IUSERNETWORK_HH_
+# define IUSERNETWORK_HH_
+
+class IUserNetwork {};
+
+#endif /* !IUSERNETWORK_HH_ */
