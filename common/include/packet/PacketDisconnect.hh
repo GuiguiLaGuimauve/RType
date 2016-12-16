f2a879811fe9934686ec6304d23cd9738ddc11dd
Maxou <baconn_m@epitech.eu>
2016-12-16 12:36:51 +0100

    Packet Disconnect

---
+++ common/include/packet/PacketDisconnect.hh
@@ -0,0 +1,27 @@
+//
+// This file was auto-generated, please do not edit it !
+//
+
+#ifndef __PACKETDISCONNECT_HH__
+#define __PACKETDISCONNECT_HH__
+
+#include <stdint.h>
+#include "APacket.hh"
+
+namespace Packet {
+
+	class PacketDisconnect : public APacket {
+
+	public:
+		PacketDisconnect();
+		PacketDisconnect(const uint8_t *data);
+		~PacketDisconnect();
+
+		bool isTcp() const;
+		bool isUdp() const;
+
+	protected:
+	};
+};
+
+#endif
