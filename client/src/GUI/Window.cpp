e90628589f49a3cb4544903bfc5293cd29a3d165
bertho_i <simon.bertho@epitech.eu>
2016-12-16 16:37:42 +0100

    test

--- client/src/GUI/Window.cpp
+++ client/src/GUI/Window.cpp
@@ -24,6 +24,7 @@ Window::~Window()
 IWidget*    Window::addWidget(int x, int y, int w, int h)
 {
   IWidget *widget = new Widget(_win, x, y, w, h);
+  widgets->setEventQueue(_queue);
   _list.push_back(widget);
   return (widget);
 }
