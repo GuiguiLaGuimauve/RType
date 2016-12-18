0a509c0089090a04f017c78b249040f539d81bf8
Simon Bertho <simon.bertho@epitech.eu>
2016-12-18 17:04:08 +0100

    touche q ajouté pour quitter

--- client/src/GUI/GuiEventManager.cpp
+++ client/src/GUI/GuiEventManager.cpp
@@ -11,6 +11,7 @@ GuiEventManager::GuiEventManager(sf::Window *w):
   _keys[sf::Keyboard::Up] = KEY_UP;
   _keys[sf::Keyboard::Space] = KEY_ATTACK;
   _keys[sf::Keyboard::Escape] = KEY_QUIT;
+  _keys[sf::Keyboard::Q] = KEY_QUIT;
 }
 
 GuiEventManager::~GuiEventManager()
