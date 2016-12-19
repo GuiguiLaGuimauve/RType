50fc63de35db7d52461e8cf985aa61869f0669fe
Maxime Baconnais <m.baconnais@outlook.com>
2016-12-19 15:08:45 +0100

    Windows Warning fix

--- common/src/packet/PacketConnect.cpp
+++ common/src/packet/PacketConnect.cpp
@@ -5,7 +5,10 @@
 #include <iostream>
 #include "PacketConnect.hh"
 #include <ctime>
+
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
 #pragma warning(disable : 4996)
+#endif
 
 PacketConnect::PacketConnect()
 {
