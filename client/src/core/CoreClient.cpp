09997e64da78f2a001cc04d6c06b07d8f2a8f044
Maxime Lecoq <maxime.lecoq@epitech.eu>
2016-12-20 21:23:04 +0100

    d

--- client/src/core/CoreClient.cpp
+++ client/src/core/CoreClient.cpp
@@ -5,7 +5,7 @@
 // Login   <maxime.lecoq@epitech.eu>
 // 
 // Started on  Fri Dec  2 14:38:54 2016 Maxime Lecoq
-// Last update Tue Dec 20 21:12:00 2016 lecoq
+// Last update Tue Dec 20 21:19:57 2016 lecoq
 //
 
 #include	"CoreClient.hh"
@@ -236,8 +236,7 @@ bool		CoreClient::rooms(const IPacket *pa, IUserNetwork *u)
   PacketRooms	*p = (PacketRooms *)pa;
 
   _gui->setRooms(p->getRooms());
-  if (_status == "login")
-    _gui->displayMenu();
+  _gui->displayMenu();
   _status = "rooms";
   (void)u;
   return (true);
