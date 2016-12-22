f16da3762ece59744f09da3f178142efb3d822aa
GuiguiLaGuimauve <guilhem.remburre@epitech.eu>
2016-12-22 12:29:30 +0100

    Comment

--- common/include/tools/AError.hh
+++ common/include/tools/AError.hh
@@ -5,7 +5,7 @@
 // Login   <lecoq_m@epitech.net>
 //
 // Started on  Mon Apr 18 13:13:17 2016 Maxime LECOQ
-// Last update Sun Nov 13 21:21:12 2016 La Guimauve
+// Last update Wed Dec 21 13:27:19 2016 La Guimauve
 //
 
 #ifndef		__AERROR_HH__
@@ -17,28 +17,68 @@
 # include		<string>
 
 namespace Error {
-  /*
-  en :AError abstract class implement IError class methods
-  fr :L'abstraction AError implémente les méthodes de IError. */
+  /*!
+		\class AError AError.hh
+		\brief classe abstraite qui implémente les methodes de IError
+	*/
   class		AError : public IError, public std::exception
   {
   private:
-    /*
-    en :_err contains the error message
-    fr :_err est un attribut contenant le message d'érreur. */
-    std::string	_err;
-    /*
-    en :_class contains the name of the class
-    fr :_class est un attribut contenant le nom de la classe. */
-    std::string	_class;
+    std::string	_err; /** \brief _err : contient le message d'erreur */
+    std::string	_class;  /** \brief _class : contient le nom de la classe */
 public:
+  /**
+  * \fn AError(const std::string &, const std::string &)
+  * \brief Constructeur du de AError avec spécification du message et de la classe
+  *
+  * \param err message d'erreur
+  * \param class nom de la classe
+  */
     AError(const std::string &, const std::string &);
+    /**
+    * \fn AError(const std::string &)
+    * \brief Constructeur du de AError avec spécification du message
+    *
+    * \param err message d'erreur
+    */
     AError(const std::string &);
+    /**
+    * \fn AError(const AError &)
+    * \brief Constructeur par copie
+    *
+    * \param err : classe AError à copier
+    */
     AError(const AError &);
+    /**
+    * \fn AError &operator=(const AError &)
+    * \brief Surcharge de l'opérateur = pour la copie d'erreur
+    *
+    * \param err : classe AError à copier
+    *
+    * \return : Instance de AError
+    */
     AError			&operator=(const AError &);
+    /**
+    * \fn const char *what() const throw()
+    * \brief Récupère le message d'erreur
+    *
+    * \return : Instance du message d'erreur
+    */
     const char			*what() const throw();
+    /**
+    * \fn void what() const
+    * \brief Affiche l'erreur et quitte le programme
+    */
     void				quit() const;
+    /**
+    * \fn void error() const
+    * \brief Affiche l'erreur
+    */
     void				error() const;
+    /**
+    * \fn ~AError() throw()
+    * \brief Destructeur de AError
+    */
     virtual ~AError() throw() {};
   };
 };
--- common/include/tools/Crypt.hh
+++ common/include/tools/Crypt.hh
@@ -5,7 +5,7 @@
 // Login   <rembur_g@epitech.eu>
 //
 // Started on  Mon Dec 19 23:31:10 2016 La Guimauve
-// Last update Tue Dec 20 23:16:56 2016 lecoq
+// Last update Wed Dec 21 15:46:19 2016 La Guimauve
 //
 
 #ifndef _CRYPT_HH_
@@ -14,15 +14,39 @@
 #include <iostream>
 #include "sha1.hh"
 
+/*!
+  \class Crypt Crypt.hh
+  \brief classe pour le chiffrer une string
+*/
 class Crypt
 {
 private:
-  char _key;
-  SHA1	_sha;
+  char _key; /** \brief _key : contient la clé pour le xor */
+  SHA1	_sha; /** \brief _sha : instance de l'objet SHA1 qui permet le hash */
 public:
+  /**
+  * \fn Crypt()
+  * \brief Constructeur de la classe
+  */
   Crypt() : _key('R') {};
 
+  /**
+  * \fn _sha1(const std::string &)
+  * \brief Chiffre la chaine de caractère en paramètre avec un hash sha1
+  *
+  * \param to_hash chaine de caractère à hash en sha1
+  *
+  * \return Une instance de la chaine de caractère hashé
+  */
   std::string _sha1(const std::string &);
+  /**
+  * \fn _xor(const std::string &)
+  * \brief Chiffre la chaine de caractère en paramètre avec un xor
+  *
+  * \param to_hash chaine de caractère à chiffrer xor
+  *
+  * \return Une instance de la chaine de caractère chiffré
+  */
   std::string _xor(std::string);
 };
 
