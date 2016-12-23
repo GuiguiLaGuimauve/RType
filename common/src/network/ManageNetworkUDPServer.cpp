9da944e8250fa02c308b721039a4fe8124a45287
dufrenb <julien1.dufrene@epitech.eu>
2016-12-23 13:05:42 +0100

    fix user sans udp_data

--- common/src/network/ManageNetworkUDPServer.cpp
+++ common/src/network/ManageNetworkUDPServer.cpp
@@ -127,7 +127,11 @@ std::vector<IUserNetwork *>	ManageNetworkUDPServer::exec()
 	    {
 	      if (_user[i]->getIp() == u->getIp())
 		{
-		  u->setPseudo(_user[i]->getPseudo());
+			if (_user[i]->getPseudo().compare("Accept") == 0)
+			{
+				u->setPseudo("accepted");
+				u->setPort(u->getPort());
+			}
 		  while (_user[i]->haveSomethingToWrite() == true)
 		    u->pushBufferWrite(_user[i]->popBufferWrite());
 		  _user[i] = u;
