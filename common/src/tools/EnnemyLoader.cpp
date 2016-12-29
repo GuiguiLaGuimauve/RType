a1248731661b7d98ffc1c7a38e03084acc981340
GuiguiLaGuimauve <guilhem.remburre@epitech.eu>
2016-12-29 10:00:35 +0100

    Compilation UNIX

--- common/src/tools/EnnemyLoader.cpp
+++ common/src/tools/EnnemyLoader.cpp
@@ -5,20 +5,22 @@
 // Login   <rembur_g@epitech.eu>
 //
 // Started on  Wed Dec 28 09:24:15 2016 La Guimauve
-// Last update Wed Dec 28 22:25:56 2016 La Guimauve
+// Last update Thu Dec 29 10:00:12 2016 La Guimauve
 //
 
 #include "EnnemyLoader.hh"
 
 IElement &EnnemyLoader::LoadEnnemy(const std::string &lib)
 {
+try {
+
 	IElement *tmp = new Ennemy(0, 0);
 	#ifndef _WIN32
 		DLloaderUnix<uint16_t()> dl_ui16;
 		DLloaderUnix<int32_t()> dl_i32;
 		DLloaderUnix<uint32_t()> dl_ui32;
 		DLloaderUnix<std::string()> dl_s;
-		DLloaderUnix<std::vector<std::pair<uint16_t, uint16_t>>()> dl_v;
+		DLloaderUnix<std::vector<std::pair<int16_t, int16_t>>()> dl_v;
 	#else
 		DLloaderWin<uint16_t()> dl_ui16;
 		DLloaderWin<int32_t()> dl_i32;
@@ -27,7 +29,6 @@ IElement &EnnemyLoader::LoadEnnemy(const std::string &lib)
 		DLloaderWin<std::vector<std::pair<int16_t, int16_t>>()> dl_v;
 	#endif //_WIN32
 
-	try {
 		std::cout << "We looking for " << ENNEMY_LIB_PATH + lib << std::endl;
 		dl_ui16.loadLib(ENNEMY_LIB_PATH + lib);
 		dl_i32.loadLib(ENNEMY_LIB_PATH + lib);
@@ -50,8 +51,8 @@ IElement &EnnemyLoader::LoadEnnemy(const std::string &lib)
 		tmp->setSpriteName(dl_s._func());
 		dl_v.extractLib("getPositions");
 		tmp->setPositions(dl_v._func());
+		return (*tmp);
 	} catch (const AError &e) {
 		std::cerr << "Exception : " << e.what() << std::endl;
 	}
-	return (*tmp);
 }
