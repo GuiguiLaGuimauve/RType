fbeceed01d1a110d7c1320d5ee65d3155349e6af
Maxime Lecoq <lecoqmaximeepitech@gmail.com>
2016-12-31 00:21:21 +0100

    d

--- common/include/packet_data/DataShoot.hpp
+++ common/include/packet_data/DataShoot.hpp
@@ -14,21 +14,18 @@ namespace Packet {
 
 	public:
 		DataShoot() {};
-	  DataShoot(const uint16_t &x, const uint16_t &y) : _name("plop"), _x(x), _y(y), _damage(10) {};
+	  DataShoot(const uint16_t &x, const uint16_t &y) :  _x(x), _y(y), _damage(10) {};
 		~DataShoot() {};
 
-		std::string getName() const { return (_name); };
 		uint16_t getX() const { return (_x); };
 		uint16_t getY() const { return (_y); };
 		uint8_t getDamage() const { return (_damage); };
 
-		void setName(const std::string & name) { _name = name; };
 		void setX(const uint16_t & x) { _x = x; };
 		void setY(const uint16_t & y) { _y = y; };
 		void setDamage(const uint8_t & damage) { _damage = damage; };
 
 	private:
-		std::string _name;
 		uint16_t _x;
 		uint16_t _y;
 		uint8_t _damage;
