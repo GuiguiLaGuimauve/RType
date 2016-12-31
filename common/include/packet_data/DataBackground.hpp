57a0a5a0ae3d312f33c5335f196c585eb78ba7e8
Maxime Baconnais <m.baconnais@outlook.com>
2016-12-31 22:35:31 +0100

    DataBackground update

--- common/include/packet_data/DataBackground.hpp
+++ common/include/packet_data/DataBackground.hpp
@@ -16,17 +16,17 @@ namespace Packet {
 		~DataBackground() {};
 
 		uint8_t getType() const { return (_type); };
-		uint16_t getX() const { return (_x); };
-		uint16_t getY() const { return (_y); };
+		int16_t getX() const { return (_x); };
+		int16_t getY() const { return (_y); };
 
 		void setType(const uint8_t & type) { _type = type; };
-		void setX(const uint16_t & x) { _x = x; };
-		void setY(const uint16_t & y) { _y = y; };
+		void setX(const int16_t & x) { _x = x; };
+		void setY(const int16_t & y) { _y = y; };
 
 	private:
 		uint8_t _type;
-		uint16_t _x;
-		uint16_t _y;
+		int16_t _x;
+		int16_t _y;
 	};
 };
 
