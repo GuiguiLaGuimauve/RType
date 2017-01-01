32d6e91354d11fde9459cb104c74d3903b46ac41
Maxime Baconnais <m.baconnais@outlook.com>
2017-01-01 19:36:24 +0100

    Add missing file

---
+++ ennemies_lib/include/BydosShoot.hpp
@@ -0,0 +1,19 @@
+
+#ifndef __BYDOSSHOOT_HH__
+#define __BYDOSSHOOT_HH__
+
+#include <stdint.h>
+#include <string>
+#include "AData.hh"
+
+
+class BydosShoot : public DataShoot {
+
+public:
+	BydosShoot() : DataShoot() { _damage = 10;};
+	virtual void move() { _x += -10; };
+protected:
+	uint8_t _damage;
+};
+
+#endif
