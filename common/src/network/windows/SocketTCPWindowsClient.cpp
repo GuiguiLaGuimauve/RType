e0b36a7e1bb2a5c6877017da7c4a1a87a8e8db21
dufren_b <julien1.dufrene@epitech.eu>
2017-01-02 04:20:49 +0100

    fix freeze windows?

--- common/src/network/windows/SocketTCPWindowsClient.cpp
+++ common/src/network/windows/SocketTCPWindowsClient.cpp
@@ -5,14 +5,22 @@
 // Login   <dufren_b@epitech.net>
 // 
 // Started on  Fri Oct 14 15:52:42 2016 julien dufrene
-// Last update Fri Dec 23 07:23:53 2016 julien dufrene
+// Last update Mon Jan  2 04:20:10 2017 julien dufrene
 //
 
 #include "SocketTCPWindowsClient.hh"
 
 using namespace Network;
 
-SocketTCPWindowsClient::SocketTCPWindowsClient() : SocketTCPWindows() {}
+SocketTCPWindowsClient::SocketTCPWindowsClient() : SocketTCPWindows()
+{
+  int		ret;
+  u_long	iMode;
+
+  iMode = 1;
+  if ((ret = ioctlsocket(m_socket, FIONBIO, &iMode)) != NO_ERROR)
+    throw ErrorSocket("Error on ioctlsocket(): " + ret);
+}
 
 bool			SocketTCPWindowsClient::connectIt(const std::string &ip, const uint32_t &port)
 {
@@ -31,8 +39,26 @@ bool			SocketTCPWindowsClient::connectIt(const std::string &ip, const uint32_t &
 	}
 	if (WSAConnect(_sock, (struct sockaddr *)&s_in, sizeof(s_in), NULL, NULL, NULL, NULL) == SOCKET_ERROR)
 	{
-		std::cerr << "Error on WSASocket(): " << WSAGetLastError() << std::endl;
-		return (false);
+		// std::cerr << "Error on WSASocket(): " << WSAGetLastError() << std::endl;
+		// return (false);
+	  if (WSAGetLastError() == WSAEINPROGRESS)
+	    {
+	      FD_ZERO(&write);
+	      FD_SET(_sock, &write);
+	      if (select(_sock + 1, NULL, &write, NULL, &tv) != -1)
+		{
+		  len = sizeof (s_in);
+		  if (getpeername(_sock, (struct sockaddr *)&s_in, &len) == WSAENOTCONN)
+		    return (false);
+		  else
+		    {
+		      std::cout << "timeout: " << c.getTimeMilli() << "ms" << std::endl;
+		      if (c.getTimeMilli() < 100)
+			return (true);
+		    }
+		}
+	    }
+	  return (false);
 	}
 	return (true);
 }
